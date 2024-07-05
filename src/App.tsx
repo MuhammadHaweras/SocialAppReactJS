f3fa2cb7aed3769656e70e402f7dc8bab9df29f9
MuhammadHaweras <haweras.noor@gmail.com>
2024-07-05 15:11:34 +0500

    signup UI and appwrite setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import React from "react";
 import "./globals.css";
+import { Route, Routes } from "react-router-dom";
+import SignInForm from "./_auth/forms/SignInForm";
+
+import SIgnUpForm from "./_auth/forms/SIgnUpForm";
+import AuthLayout from "./_auth/forms/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages/Home";
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello World
-    </h1>
-  )
+    <main className="flex h-screen">
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+
+          <Route path="/sign-up" element={<SIgnUpForm />} />
+        </Route>
+
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
